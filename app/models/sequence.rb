5a074c6c9442f432252a0a1f2191c923b0943a8a
meditate <aroundofyou@gmail.com>
2019-06-09 17:12:51 +0200

    Add logic to Sequence model

--- app/models/sequence.rb
+++ app/models/sequence.rb
@@ -4,8 +4,26 @@ class Sequence < ApplicationRecord
   has_many :sequences_throws
   has_many :throws, through: :sequences_throws
 
-  enum status: { opened: 0, finished: 1 }
+  scope :active_sequences, -> { where(status: 0) }
+
+  enum status: { active: 0, finished: 1 }
 
   validates :status, presence: true
   validates :number_of_throws, inclusion: 2..3, presence: true
+  
+  def self.active_for_user_in_game(user, game)
+    where(frame: user.frames.where(game_id: game.id)).active_sequences 
+  end
+
+  def completed_with_throws?
+    throws.count == number_of_throws
+  end
+  
+  def score
+    throws.sum(&:score)
+  end
+
+  def complete!
+    finished! && frame.complete!
+  end
 end
