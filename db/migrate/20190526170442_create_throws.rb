1ccd23e84a0134aeec2c8ccadd324b497a5129e3
meditate <aroundofyou@gmail.com>
2019-06-09 17:15:33 +0200

    Refactor throw_spec
    Change create_throw migration to set default

--- db/migrate/20190526170442_create_throws.rb
+++ db/migrate/20190526170442_create_throws.rb
@@ -4,7 +4,7 @@ class CreateThrows < ActiveRecord::Migration[5.0]
       t.integer :score, null: false, default: 0
       t.string :presentation
       t.references :frame, foreign_key: true
-      t.integer :attempt_number, null: false
+      t.integer :attempt_number, null: false, default: 1
 
       t.timestamps
     end
--- spec/models/throw_spec.rb
+++ spec/models/throw_spec.rb
@@ -3,8 +3,6 @@ require "rails_helper"
 RSpec.describe Throw, type: :model do
   describe "validations" do
     it { is_expected.to validate_presence_of(:score) }
-    it { is_expected.to validate_presence_of(:attempt_number) }
     it { is_expected.to validate_inclusion_of(:score).in_range(0..10) }
-    it { is_expected.to validate_inclusion_of(:attempt_number).in_range(1..3) }
   end
 end
