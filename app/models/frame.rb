b2c2feb3f5ab749ea42dd17f562822347aa65ec5
meditate <aroundofyou@gmail.com>
2019-06-09 17:10:53 +0200

    Remove :name from Frame

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -3,9 +3,9 @@ class Frame < ApplicationRecord
   belongs_to :user
 
   has_one :sequence
-  has_many :throws
+  has_many :throws, dependent: :destroy
 
-  acts_as_list scope: [:game, :user]
+  acts_as_list scope: [:game_id, :user_id]
 
   enum status: { current: 0, finished: 1 }
 
@@ -17,25 +17,11 @@ class Frame < ApplicationRecord
     game.score_for(user)
   end
 
-  def name
-    name = 
-      if frame.score == 10
-        if frame.throws.count == 2
-          :spare
-        elsif frame.throws.count == 1
-          :strike
-        end
-      end
-    name || :basic
-  end
-
   def complete!
-    game_score.add_score(score)
+    finished! && game_score.add_score(score)
   end
 
   def last_in_game?
     game.frames_count == position
   end
 end
-
-# Reference score with user and game, and update it on sequence finish
