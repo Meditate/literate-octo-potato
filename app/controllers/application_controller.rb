2681d5364d2826f1f349539f0498f309284e2821
meditate <aroundofyou@gmail.com>
2019-06-09 17:03:03 +0200

    Add authentication by api_key

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::API
+  def authenticate_user
+    api_key = params[:api_key] || request.headers["HTTP_AUTHORIZATION"]
+
+    raise ArgumentError, "no api key provided" if api_key.blank?
+
+    @current_user = User.find_by(api_key: api_key)
+
+    raise ArgumentError, "Invalid user" if @current_user.nil?
+  end
 end
